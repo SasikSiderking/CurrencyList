5d889b928f7e7128cd7120e3a2efd7e1c659324d
-_- <Sidrsidsashok@mail.ru>
2022-05-13 12:47:19 +0700

    слияние с конвертором

--- app/src/main/java/com/example/currencylist/fragments/calculator.java
+++ app/src/main/java/com/example/currencylist/fragments/calculator.java
@@ -10,12 +10,14 @@ import android.view.ViewGroup;
 
 import com.example.currencylist.R;
 
-public class ChartFragment extends Fragment {
+public class calculator extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chart, container, false);
+        return inflater.inflate(R.layout.calculator, container, false);
     }
+
+
 }
