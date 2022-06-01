e35d3aa7d17671213fb9873346d11439904be89b
-_- <Sidrsidsashok@mail.ru>
2022-06-01 21:05:18 +0700

    Data reloading with service, network class

--- app/src/main/java/com/example/currencylist/piapsTesting/ErrorService.java
+++
@@ -1,5 +0,0 @@
-package com.example.currencylist.piapsTesting;
-
-public interface ErrorService {
-    void errorLog(String error);
-}
--- app/src/main/java/com/example/currencylist/piapsTesting/MockErrorService.java
+++
@@ -1,9 +0,0 @@
-package com.example.currencylist.piapsTesting;
-
-public class MockErrorService implements ErrorService {
-    public String error;
-
-    public void errorLog(String error){
-        this.error = error;
-    }
-}
